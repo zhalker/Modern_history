8499b7fad510c7b8ce7a806d0ce0f58b8ca3ab93
zhalker <49279896+zhalker@users.noreply.github.com>
2024-04-14 23:55:14 +0200

    Added new usage examples
    New examples are added for unique events. For example "on:back", "on:forward", "on:go"

--- example.js
+++ example.js
@@ -1,9 +1,20 @@
-let event_identifier = window.Modern_history.change((url_old, url_new)=>{
+let event_change_identifier = window.Modern_history.change((url_old, url_new)=>{
 	console.log("old: ",url_old,"new:", url_new)
 })
 
-window.Modern_history.replaceState({},"www.google.com")
+let event_on_back_identifier = window.Modern_history.on("change", "back", (url_old, url_new) => {
+    console.log("old: ", url_old, "new:", url_new)
+})
 
-window.Modern_history.changeOff(event_identifier)
+window.Modern_history.replaceState({},"/page_1")
 
-window.Modern_history.replaceState({},"www.youtube.com")
+window.Modern_history.changeOff(event_change_identifier)
+
+window.Modern_history.replaceState({},"/page_2")
+
+window.Modern_history.back()
+
+window.Modern_history.changeOff(event_on_back_identifier)
+
+window.Modern_history.back()
+
